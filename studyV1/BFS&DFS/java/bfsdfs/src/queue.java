b7c000a0f76d6ae7b4f0ed96dcb4382690e35e5a
Sun Yerim <syl1602@naver.com>
2023-07-29 16:14:01 +0900

    [07-29] BFS&DFS JAVA

---
+++ studyV1/BFS&DFS/java/bfsdfs/src/queue.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class queue {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+
+        // 삽입(5) - 삽입(2) - 삽입(3) - 삽입(7) - 삭제() - 삽입(1) - 삽입(4) - 삭제()
+        q.offer(5); // 삽입
+        q.offer(2);
+        q.offer(3);
+        q.offer(7);
+        q.poll(); // 삭제
+        q.offer(1);
+        q.offer(4);
+        q.poll();
+
+        while (!q.isEmpty()) {
+            System.out.println(q.poll());
+        }
+    }
+
+}
